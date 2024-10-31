4a442c96e63bd1fda1d1c2cd1ebba1f2952654ee
maldonae <maldonadoeric69120@gmail.com>
2024-10-31 18:57:50 +0100

    serveur envoi hello world

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+const port = 3310;
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
